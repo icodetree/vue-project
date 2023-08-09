2e0b8ba629c55c35a6821bbef87420c6d71854f6
icodetree <icodetree@naver.com>
2023-08-09 23:34:14 +0900

    add monaco test

--- vue3-vite/src/views/AboutView.vue
+++ vue3-vite/src/views/AboutView.vue
@@ -1,5 +1,34 @@
-<script setup></script>
 <template>
-  <h1>About Page</h1>
-  <p>{{ $route.path }}</p>
+  <monaco-editor v-model="code" :options="editorOptions"></monaco-editor>
 </template>
+
+<script>
+import { ref } from 'vue';
+import MonacoEditor from '@guolao/vue-monaco-editor';
+
+export default {
+  components: {
+    MonacoEditor,
+  },
+
+  setup() {
+    const code = ref(
+      `import ButtonFullArea from '@/components/libs/button/ButtonFullArea.vue';`,
+    );
+    const editorOptions = {
+      language: 'javascript',
+      theme: 'vs-dark',
+      automaticLayout: true,
+      formatOnType: true,
+      formatOnPaste: true,
+    };
+
+    console.log(code.value);
+
+    return {
+      code,
+      editorOptions,
+    };
+  },
+};
+</script>
