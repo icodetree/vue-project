857fe8b99cc99658ff0be86f6c90173b22d59117
icodetree <icodetree@naver.com>
2023-09-01 23:03:04 +0900

    add

--- vue3-vite/src/views/HomeView.vue
+++ vue3-vite/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <h1>Home Page</h1>
   <p>{{ $route.name }}</p>
 
-  <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+  <button class="btn btn-primary" @click="goAboutPage">About</button>
 
   <hr class="my-4" />
   <div class="row g-3">
