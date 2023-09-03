f6a6b0f66d9f35cc507fb33b6f5c920fe9708ead
icodetree <icodetree@naver.com>
2023-09-03 23:19:04 +0900

    put 을 patch로 변경해주기

--- vue3-vite/src/api/posts.js
+++ vue3-vite/src/api/posts.js
@@ -13,8 +13,14 @@ export function createPost(data) {
   return posts.post('', data);
 }
 
+// export function updatePost(id, data) {
+//   return posts.put(`/${id}`, data);
+// }
+
+// put의 용도는 데이터 전체를 수정하는 메서드
+// 일부를 수정할때 사용하는 메서드(patch)로 변경해준다.
 export function updatePost(id, data) {
-  return posts.put(`/${id}`, data);
+  return posts.patch(`/${id}`, data);
 }
 
 export function deletePost(id) {
