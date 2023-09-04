efbbf8d78a9837021c9605efe56962a5e2a8b112
icodetree <icodetree@naver.com>
2023-09-05 00:21:24 +0900

    add alert

--- vue3-vite/src/components/AppAlert.vue
+++ vue3-vite/src/components/AppAlert.vue
@@ -1,4 +1,35 @@
 <template>
-  <div class="alert alert-success" role="alert">수정이 완료되었습니다.</div>
+  <div v-if="show" class="app-alert alert" role="alert" :class="styleClass">
+    {{ message }}
+  </div>
 </template>
-<script></script>
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'error',
+    validator: value => ['success', 'error'].includes(value),
+  },
+});
+const styleClass = computed(() =>
+  props.type === 'error' ? 'alert-danger' : 'alert-success',
+);
+</script>
+
+<style scoped>
+.app-alert {
+  position: fixed;
+  top: 10px;
+  right: 10px;
+}
+</style>
--- vue3-vite/src/views/posts/PostEditView.vue
+++ vue3-vite/src/views/posts/PostEditView.vue
@@ -18,7 +18,7 @@
         <button class="btn btn-primary">수정</button>
       </template></PostForm
     >
-    <AppAlert />
+    <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
   </div>
 </template>
 <script setup>
@@ -26,7 +26,7 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
-import { AppAlert } from '@/components/AppAlert.vue';
+import AppAlert from '@/components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -43,6 +43,7 @@ const fetchPost = async () => {
     setForm(data);
   } catch (error) {
     console.error(error);
+    vAlert('네트워크 오류!');
   }
 };
 const setForm = ({ title, content }) => {
@@ -54,11 +55,26 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    router.push({ name: 'PostDetail', params: { id } });
+    // router.push({ name: 'PostDetail', params: { id } });
+    vAlert('수정이 완료되었습니다.', 'success');
   } catch (error) {
     console.error(error);
   }
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+// alert
+const showAlert = ref(false);
+const alertMessage = ref('');
+const alertType = ref('');
+
+const vAlert = (message, type = 'error') => {
+  showAlert.value = true;
+  alertMessage.value = message;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
+};
 </script>
