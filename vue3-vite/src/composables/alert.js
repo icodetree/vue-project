ef20a0e14feca0f9be21707e021c2038efdcc221
icodetree <icodetree@naver.com>
2023-09-29 23:17:51 +0900

    add

--- vue3-vite/src/composables/alert.js
+++ vue3-vite/src/composables/alert.js
@@ -1,6 +1,7 @@
 import { ref } from 'vue';
 const alerts = ref([]);
 
+// composabls function
 export function useAlert() {
   const vAlert = (message, type = 'error') => {
     alerts.value.push({ message, type });
